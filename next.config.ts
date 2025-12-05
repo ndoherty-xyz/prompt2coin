927e48ae0d6dfab49c63d363bf562744cb1208ff
ndoherty <ndoherty.xyz@gmail.com>
2025-12-04 20:06:55 -0700

    fix build

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://zora-gen-cdn.up.railway.app/**")],
   },
+
+  // Needed for a wallet connect issue
+  // https://github.com/vercel/next.js/issues/86099
+  // https://github.com/pinojs/thread-stream/issues/183
+  // https://github.com/WalletConnect/walletconnect-monorepo/issues/7080
+  serverExternalPackages: ["pino", "pino-pretty", "thread-stream"],
 };
 
 export default nextConfig;
